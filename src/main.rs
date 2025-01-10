c7f20972ca7c4c1fee7f12b384ebaadc8d3eb5a9
Pau Ruiz Safont <unduthegun@gmail.com>
2025-01-10 11:34:20 +0000

    solved

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,45 @@
-fn unique(a: Vec<i32>) -> Vec<i32> {
-    todo!()
+fn unique_norm(a: Vec<i32>) -> Vec<i32> {
+    let mut deduped = a.clone();
+    deduped.sort_unstable();
+    deduped.dedup();
+    deduped
 }
 
 // advanced 1: use generic types
-// fn unique(a: Vec<T>) -> Vec<T> {
-//     todo!();
-// }
+fn unique_generic<T>(a: Vec<T>) -> Vec<T>
+where T:Clone, T:Ord
+{
+    let mut deduped = a.clone();
+    deduped.sort_unstable_by(|x: &T, y: &T| x.cmp(y));
+    deduped.dedup();
+    deduped
+}
 
 // advanced 2: keep items in order
-// fn unique(a: Iterable<T>) -> Vec<T> {
-//     todo!();
-// }
+fn unique_order<V, T>(a: V) -> Vec<T>
+where V:IntoIterator<Item = T>, T:PartialEq
+{
+    let mut deduped: Vec<T> = Vec::new();
+    for elem in a {
+        if !deduped.contains(&elem) {
+            deduped.push(elem);
+        }
+    }
+    deduped
+}
 
 // advanced 3: use iterators
-// fn unique(a: Iterable<T>) -> Vec<T> {
-//     todo!();
-// }
+fn unique<V, T>(a: V) -> Vec<T>
+where V:IntoIterator<Item = T>, T:PartialEq
+{
+    let mut deduped: Vec<T> = Vec::new();
+    for elem in a {
+        if !deduped.contains(&elem) {
+            deduped.push(elem);
+        }
+    }
+    deduped
+}
 
 fn main() {
     let input = vec![2, 1, 1];
@@ -25,7 +49,7 @@ fn main() {
 
 #[test]
 fn empty_list() {
-    let input = vec![];
+    let input:Vec<i32> = vec![];
     let expected_output = vec![];
     let actual_output = unique(input);
     assert_eq!(actual_output, expected_output);
