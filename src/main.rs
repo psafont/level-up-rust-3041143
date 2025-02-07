65ee41891197f531ccbddff55580be2cbcc03f95
Pau Ruiz Safont <unduthegun@gmail.com>
2025-02-07 14:43:27 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use chrono::NaiveDate;
 
 /// Parses a string that represents a date. When a date
-/// is unable to be determined, return `None`. 
+/// is unable to be determined, return `None`.
 fn flexible_date_parse(text: &str) -> Option<NaiveDate> {
     todo!();
 }
@@ -18,32 +18,41 @@ fn main() {
     for d in dates.iter() {
         println!("{} -> {:?}", d, flexible_date_parse(d));
     }
-
 }
 
 #[test]
 fn ymd_hyphen() {
-    assert_eq!(flexible_date_parse("2010-12-11"), Some(NaiveDate::from_ymd(2010, 12, 11)))
+    assert_eq!(
+        flexible_date_parse("2010-12-11"),
+        NaiveDate::from_ymd_opt(2010, 12, 11)
+    )
 }
 
 #[test]
 fn ymd_slash() {
-    assert_eq!(flexible_date_parse("1999/Mar/02"), Some(NaiveDate::from_ymd(1999, 3, 2)))
+    assert_eq!(
+        flexible_date_parse("1999/Mar/02"),
+        NaiveDate::from_ymd_opt(1999, 3, 2)
+    )
 }
 
 #[test]
 fn dmy_dot() {
-    assert_eq!(flexible_date_parse("01.Mar.2021"), Some(NaiveDate::from_ymd(2021, 3, 1)))
+    assert_eq!(
+        flexible_date_parse("01.Mar.2021"),
+        NaiveDate::from_ymd_opt(2021, 3, 1)
+    )
 }
 
 #[test]
 fn mdy_dot() {
-    assert_eq!(flexible_date_parse("Apr.05.2021"), Some(NaiveDate::from_ymd(2021, 4, 5)))
+    assert_eq!(
+        flexible_date_parse("Apr.05.2021"),
+        NaiveDate::from_ymd_opt(2021, 4, 5)
+    )
 }
 
 #[test]
 fn invalid() {
     assert_eq!(flexible_date_parse("not a date"), None)
 }
-
-
