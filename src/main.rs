2e0efd20edbb644ee118cdcd3bec92e40ac5b972
Pau Ruiz Safont <unduthegun@gmail.com>
2025-02-07 14:42:50 +0000

    solved

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,36 @@
 mod run_length_encoding {
     pub fn encode(text: &str) -> String {
-        todo!()
+        let mut last_n = 1;
+        let mut chars = text.chars();
+        let mut last_maybe = chars.next();
+        let mut encoded = String::with_capacity(text.len());
+
+        while let Some(last) = last_maybe {
+            let next = chars.next();
+
+            if next == Some(last) && last_n != 9 {
+                last_n += 1;
+            } else {
+                let chunk = format!("{}{}", last_n, last);
+                encoded.push_str(&chunk);
+                last_n = 1;
+            }
+            last_maybe = next;
+        }
+        encoded
     }
 
     pub fn decode(text: &str) -> String {
-        todo!()
+        let mut decoded = String::with_capacity(text.len());
+        let mut chars = text.chars();
+
+        while let (Some(count), Some(char)) = (chars.next(), chars.next()) {
+            let count = count.to_digit(10).unwrap();
+            for _ in 0..count {
+                decoded.push(char);
+            }
+        }
+        decoded
     }
 }
 
@@ -13,14 +39,21 @@ fn main() {
 }
 
 #[test]
-fn abc() {
+fn test_empty() {
+    use run_length_encoding::*;
+
+    assert_eq!(encode(""), "");
+}
+
+#[test]
+fn test_abc() {
     use run_length_encoding::*;
 
     assert_eq!(encode("abc"), "1a1b1c");
 }
 
 #[test]
-fn round_trip() {
+fn test_round_trip() {
     use run_length_encoding::*;
 
     let input = "LinkedIn";
@@ -29,9 +62,17 @@ fn round_trip() {
 }
 
 #[test]
-fn long_run() {
+fn test_long() {
     use run_length_encoding::*;
 
     let input = "AAAAA AAAAAAAAAA AAAAAAAAAAAAAAAAAAAA";
     assert_eq!(encode(input), "5A1 9A1A1 9A9A2A");
+}
+
+#[test]
+fn test_longnumbers() {
+    use run_length_encoding::*;
+
+    let input = "1223334444 55555666666777777788888888 9999999990000000000";
+    assert_eq!(encode(input), "112233441 556677881 999010");
 }
